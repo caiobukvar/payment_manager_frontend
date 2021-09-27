3d54c23e392b86325d16398ca17a09625185294d
Caio Bukvar <bukvar2009@gmail.com>
2021-09-27 08:00:03 -0300

    criacao SignUp e conclusão do layout

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,9 +1,42 @@
-import './styles.css'
+import './styles.css';
+import Logo from "../../assets/logo.svg";
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import InputPassword from "../../components/InputPassword/InputPassword"
 
 function SignUp() {
+    const [password, setPassword] = useState('');
+
     return (
-        <div>
-            <h2>signup</h2>
+        <div className="container-form flex-column">
+            <form className="form form-sign-up">
+                <div className="logo">
+                    <img src={Logo} alt="logo" />
+                </div>
+                <div className="flex-column border-bt">
+                    <label className="mb-md font-md-bold" htmlFor="name">Nome</label>
+                    <input type="text" id="name" placeholder="Digite seu nome" />
+                </div>
+                <div className="flex-column border-bt">
+                    <label className="mb-md font-md-bold" htmlFor="email">E-mail</label>
+                    <input type="text" id="email" placeholder="Digite seu e-mail" />
+                </div>
+                <div className="flex-column input-password border-bt">
+                    <InputPassword
+                        label="Senha"
+                        placeholder="Digite sua senha"
+                        value={password}
+                        setValue={setPassword}
+                    />
+                </div>
+                <button type="submit" className="btn-pink mb-lg mt-lg font-md-bold">
+                    Criar Conta
+                </button>
+            </form>
+            <span className="mt-lg">
+                Já possui uma conta?
+                <Link to="/sign-in"> Acesse agora!</Link>
+            </span>
         </div>
     )
 }
