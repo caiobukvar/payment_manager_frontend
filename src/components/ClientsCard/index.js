db477771a3d87c74749dc2919017f044f63257c8
Caio Bukvar <bukvar2009@gmail.com>
2021-10-18 09:40:35 -0300

    adicionados onClicks para ClientsCard

--- src/components/ClientsCard/index.js
+++ src/components/ClientsCard/index.js
@@ -2,7 +2,20 @@ import './styles.css';
 import React from 'react';
 import Clients from '../../assets/clients.svg'
 
+import { useHistory } from 'react-router-dom';
+
 function ClientsCard() {
+
+    const history = useHistory();
+
+    function handleDefaulterClients() {
+        history.push(`/relatorios?status=inadimplentes`);
+    }
+
+    function handleInDayClients() {
+        history.push(`/relatorios?status=em-dia`);
+    }
+
     return (
         <div className="card-client flex-column box-shadow mt-xxl">
             <div className="flex-row topbar background-dark-smooth card-padding content-center border-round-top">
@@ -10,11 +23,11 @@ function ClientsCard() {
                 <p className="ml-sm">Clientes</p>
             </div>
             <div className="flex-column card-content items-center content-center card-padding-lg">
-                <div className="border-green flex-row space-between pad-md items-center full-width">
+                <div className="border-green flex-row space-between pad-md items-center full-width" onClick={handleInDayClients}>
                     <p>Em dia</p>
                     <p className="font-xxl">0</p>
                 </div>
-                <div className="border-red flex-row space-between mt-md pad-md items-center full-width">
+                <div className="border-red flex-row space-between mt-md pad-md items-center full-width" onClick={handleDefaulterClients}>
                     <p>Inadimplentes</p>
                     <p className="font-xxl">0</p>
                 </div>
